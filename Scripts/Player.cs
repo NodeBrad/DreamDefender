2e989f02cda9e989e60e4fcdb8154b5a02d32906
Brad Goodall <bradgoodall94@gmail.com>
2022-08-24 10:06:49 +1000

    Player Movement and Level Blockout
    Added the player movement script and the level blockout phase

---
+++ Scripts/Player.cs
@@ -0,0 +1,120 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody
+{
+    private int maxSpeed = 12;
+    private int acceleration = 60;
+    private int friction = 50;
+    private int airFriction = 10;
+    private int jumpImpulse = 15;
+    private int gravity = -40;
+
+    private float mouseSensitivity = 0.1f;
+    private Vector3 velocity = Vector3.Zero;
+    private Vector3 snapVector = Vector3.Zero;
+    private Spatial head;
+
+    public override void _Ready()
+    {
+        head = GetNode<Spatial>("Head");
+
+        Input.MouseMode = Input.MouseModeEnum.Captured; // Capture the mouse
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        // Handle mouse window capture
+        if (@event.IsActionPressed("left_click")) // If player clicks in the window
+        {
+            Input.MouseMode = Input.MouseModeEnum.Captured; // Capture the mouse
+        }
+
+        // Toggle window capture
+        if (@event.IsActionPressed("toggle_mouse_mode"))
+        {
+            if (Input.MouseMode == Input.MouseModeEnum.Captured)
+                Input.MouseMode = Input.MouseModeEnum.Visible;
+            else
+                Input.MouseMode = Input.MouseModeEnum.Captured;
+        }
+
+        if ((@event is InputEventMouseMotion mouseMotion) && (Input.MouseMode == Input.MouseModeEnum.Captured))
+        {
+            RotateY(Mathf.Deg2Rad(-mouseMotion.Relative.x * mouseSensitivity));
+            head.RotateX(Mathf.Deg2Rad(-mouseMotion.Relative.y * mouseSensitivity));
+        }
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Vector3 inputVector = getInputVector();
+        Vector3 direction = getDirection(inputVector);
+        applyMovement(direction, delta);
+        applyFriction(direction, delta);
+        applyGravity(delta);
+        jump();
+        velocity = MoveAndSlide(velocity, Vector3.Up);
+    }
+
+    private Vector3 getInputVector()
+    {
+        Vector3 inputVector = Vector3.Zero;
+        inputVector.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        inputVector.z = Input.GetActionStrength("move_backward") - Input.GetActionStrength("move_forward");
+
+        if (inputVector.Length() > 1)
+            return inputVector.Normalized();
+        else
+            return inputVector;
+    }
+
+    private Vector3 getDirection(Vector3 inputVector)
+    {
+        Vector3 direction = Vector3.Zero;
+        direction = (inputVector.x * Transform.basis.x) + (inputVector.z * Transform.basis.z);
+        return direction;
+    }
+
+    private void applyMovement(Vector3 direction, float delta)
+    {
+        if (direction != Vector3.Zero)
+        {
+            velocity.x = velocity.MoveToward(direction * maxSpeed, acceleration * delta).x;
+            velocity.z = velocity.MoveToward(direction * maxSpeed, acceleration * delta).z;
+        }
+    }
+
+    private void applyFriction(Vector3 direction, float delta)
+    {
+        if (direction == Vector3.Zero)
+        {
+            if (IsOnFloor())
+                velocity = velocity.MoveToward(Vector3.Zero, friction * delta);
+            else
+            {
+                velocity.x = velocity.MoveToward(direction * maxSpeed, airFriction * delta).x;
+                velocity.z = velocity.MoveToward(direction * maxSpeed, airFriction * delta).z;
+            }
+        }
+    }
+
+    private void applyGravity(float delta)
+    {
+        velocity.y += gravity * delta;
+        velocity.y = Mathf.Clamp(velocity.y, gravity, jumpImpulse);
+    }
+
+    private void jump()
+    {
+        if ((Input.IsActionJustPressed("jump")) && (IsOnFloor()))
+        {
+            velocity.y = jumpImpulse;
+        }
+        if ((Input.IsActionJustReleased("jump")) && (velocity.y>jumpImpulse/2))
+        {
+            velocity.y = jumpImpulse / 2;
+        }
+    }
+
+}
